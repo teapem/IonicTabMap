f750ccc78cdc677f3d7e52f664ef2e33ce0631a5
Teape <Mike.Teape@airways.co.nz>
2016-09-14 11:20:14 +1200

    fixed map canvas problem

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -73,7 +73,7 @@ angular.module('starter.controllers', [])
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };          
              
-            var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
+            var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);          
              
             $scope.map = map;   
             $ionicLoading.hide();           
